9a3a02ad7c2d8bc5bdb4a060dfa0d18a547aeea6
nuriddinyuldashev <nuriddin.yuldashev@axtechnology.uz>
2024-09-18 11:33:51 +0500

    kysely added

--- app/connector/my_ts/functions.ts
+++ app/connector/my_ts/functions.ts
@@ -1,18 +1,23 @@
 import { db } from "./db";
 import bcrypt from "bcrypt";
+import * as sdk from "@hasura/ndc-lambda-sdk"
+
+import dotenv from 'dotenv'; 
+dotenv.config();
 
 /**
  * @readonly Exposes the function as an NDC function (the function should only query data without making modifications)
  */
 export function hello(name?: string) {
-  return `hello ${name ?? "world"}`;
+  console.log(process.env);
+  throw new sdk.Forbidden('Ooops', process.env);
+  return 'hello';
 }
 
 export async function insertUser(username: string, password: string, name: string): Promise<{id: number}> {
-  const user = await db.insertInto('users').values({
+  return db.insertInto('users').values({
     name,
     password: await bcrypt.hash(password, 8),
     username,
   }).returning('id').executeTakeFirstOrThrow();
- return user;
 }
